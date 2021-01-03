c46f97f564c7d00b5d5e7158fd10d29c51aa72f9
ThePrakharRajpali <rprakhar@iitg.ac.in>
2021-01-04 02:01:31 +0530

    imported libraries

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-const express = require("express");
+const express    = require("express");
+const mongoose   = require("mongoose");
+const bodyParser = require("body-parser");
+
+const port    = 3000;
 const app     = express();
 
+mongoose.connect('mongodb://localhost:27017/blogs_bookmark', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
     res.render("home");
-})
+});
 
 app.listen(3000, () => {
     console.log("Server started at Port:3000");
-})
+});
